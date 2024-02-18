c60a9dda2c3666d8ff932d603f09bd1dd937a485
Olena Yakovleva <alyona.jackovleva@gmail.com>
2024-02-18 14:53:51 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Користувач вводить із клавіатури три числа. Необхідно знайти суму чисел, добуток чисел. Результат обчислень вивести на екран.
+""" number1 = float (input("Enter the first number: "))
+number2 = float (input("Enter the second number: "))
+number3 = float (input ("Enter the third number: ")) 
+print ("The sum of", number1, "and", number2, "and the third number", number3, "is", number1 + number2 + number3) 
+print ("The result of multiplying the numbers is: ", number1 * number2 * number3) """
+
+""" #Користувач вводить із клавіатури три числа. Перше число — зарплата за місяць, друге число — сума місячного платежу за кредитом у банку, третє число ­— заборгованість за комунальні послуги. Необхідно вивести на екран суму, яка залишиться у користувача після всіх виплат.
+salary = float (input ("Enter your salary: $"))
+debt = float (input("How much do you owe? $"))
+debt2 = float (input ("How much you need to pay for the utilies? $"))
+print ("Remainig money after paying your debts: ", salary - (debt+debt2)) """
+
+
+#Напишіть програму, що обчислює площу ромба. Користувач вводить із клавіатури довжину двох його діагоналей.
+diagonal1 = float (input("Enter the length of the first diagonal: "))
+diagonal2 = float (input("Enter the length of the second diagonal: "))
+print  ("The area of the rhombus is: ", (diagonal1 * diagonal2)/2)
